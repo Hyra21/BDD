ba744d3937999d62b84ebc7b84829c253a177d55
Hyra21 <111527004+Hyra21@users.noreply.github.com>
2023-09-14 17:22:23 -0700

    Add files via upload

--- Practica2.sql
+++ Practica2.sql
@@ -1,3 +1,24 @@
-SELECT * FROM practica2.trabajadores;
+create database practica2;
 
-select * From trabajadores where no_Control > 90;
+use practica2;
+
+CREATE TABLE Trabajadores (
+    no_Control INT PRIMARY KEY,
+    nombre VARCHAR(25),
+    domicilio VARCHAR(30),
+    ciudad VARCHAR(15),
+    edad INT,
+    oficio VARCHAR(15)
+);
+
+INSERT INTO Trabajadores (no_Control, nombre, domicilio, ciudad, edad, oficio)
+VALUES
+    (012, 'Pedro', 'Lopez', 'Manzanita', 100, 'estudiante'),
+    (032, 'José', 'Rodriguez', 'Melon 23-4', 20, 'Abogado'),
+    (054, 'Juan', 'Jimenez', 'Lechuga', 987, 'Abogado'),
+    (078, 'Mario', 'Vazquez A. L.', 'Mandarina', 2345, 'estudiante'),
+    (029, 'Adolfo', 'Alvarez', 'Jamaica', 199, 'Abogado'),
+    (090, 'Alissa', 'Sandoval', 'Guayaba', 987, 'Secretaria'),
+    (073, 'Sandra', 'Medina.', 'Rio fresa', 200, 'Secretaria'),
+    (099, 'Carmen', 'Garcia A. 1.', 'Limones', 234, 'Secretaria'),
+    (034, 'Rocio Ortiz Pina', '987', 'Tecate', 20, 'estudiante');
